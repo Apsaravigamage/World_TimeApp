ead7d6e08b9b34073a6e5779be62493a29909873
Apsaravi Gamage <apsaravigamage@gmail.com>
2020-10-30 17:24:19 +0530

    Maps and Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:apptime/pages/home.dart';
+import 'package:apptime/pages/loading.dart';
+import 'package:apptime/pages/choose_location.dart';
+
 void main() => runApp(MaterialApp(
-  home: Home( ) ,
+  initialRoute: '/home',
+  routes: {
+    '/': (context) => Loading(),
+    '/home': (context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
 ));
+
+
+
+
 
 
